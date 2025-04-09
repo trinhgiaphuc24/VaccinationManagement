4a4e7843d9da864d230ef688c065a235524ec105
Phuc <trinhgiaphuc24@gmail.com>
2025-04-09 17:18:50 +0700

    update database

--- vaccineapp/vaccine/models.py
+++ vaccineapp/vaccine/models.py
@@ -11,7 +11,7 @@ class RoleEnum(models.TextChoices):
 
 class BaseModel(models.Model):
     name = models.CharField(max_length=100, null=False, unique=True)
-    active = models.BooleanField(default=True)
+    # active = models.BooleanField(default=True)
 
     class Meta:
         abstract = True
@@ -20,9 +20,13 @@ class BaseModel(models.Model):
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
+    sex = models.BooleanField(null=True)  # Thêm sex
+    dateOfBirth = models.DateField(null=True)
     is_active = models.BooleanField(default=True)
-    role = models.CharField(max_length=20, choices=RoleEnum.choices, default=RoleEnum.PATIENT)
-    avatar = CloudinaryField('avatar', null=True)
+    createdAt = models.DateTimeField(auto_now_add=True, null=True)
+    avatarUrl = CloudinaryField('avatar', null=True)
+    userRole = models.CharField(max_length=20, choices=RoleEnum.choices,
+                                default=RoleEnum.PATIENT)
 
     def __str__(self):
         return self.username
@@ -44,6 +48,7 @@ class Information(models.Model):
     date_of_birth = models.DateField()
     sex = models.BooleanField()
     address = models.TextField()
+    email = models.EmailField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="information")
 
     def __str__(self):
@@ -71,7 +76,7 @@ class CountryProduce(BaseModel):
 class Vaccine(BaseModel):
     description = models.TextField()
     price = models.FloatField()
-    vaccine_type = models.ForeignKey(VaccineType, on_delete=models.CASCADE,null=True,  # Cho phép giá trị NULL
+    vaccine_type = models.ForeignKey(VaccineType, on_delete=models.CASCADE,null=True,
     blank=True, related_name="vaccinetype")
     country_produce = models.ForeignKey(CountryProduce, on_delete=models.SET_NULL,
         null=True,
@@ -93,12 +98,30 @@ class Appointment(models.Model):
     date = models.DateField()
     status = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="appointments")
     health_centre = models.ForeignKey(HealthCentre, on_delete=models.CASCADE, related_name="appointments")
-    vaccines = models.ManyToManyField(Vaccine, related_name="appointments")
+    # vaccines = models.ManyToManyField(Vaccine, related_name="appointments")
     time = models.ForeignKey(Time, on_delete=models.CASCADE, related_name="appointments")
 
     def __str__(self):
         return f"Appointment on {self.date} - {self.user.username}"
 
 
+class AppointmentDetail(models.Model):
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE, related_name="appointment_details")
+    vaccine = models.ForeignKey(Vaccine, on_delete=models.CASCADE, related_name="appointment_details")
+    quantity = models.IntegerField()
+
+    def __str__(self):
+        return f"Detail for {self.appointment} - Vaccine: {self.vaccine.name}"
+
+
+class CommunicationVaccination(BaseModel):
+    date = models.DateField()
+    time = models.DateTimeField()
+    address = models.TextField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
